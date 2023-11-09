305d05d9c4834ab27439a266cb8ac2dd11089ca4
Jonathan Hill <jonathan.r.hill@outlook.com>
2023-11-09 15:26:22 +0000

    Fixed inputClientName method
    Fixed error checking and spacing issues, changed regex to be correct

--- ItineraryManager/src/main/java/d3344758/phase_2_3/ItineraryInput.java
+++ ItineraryManager/src/main/java/d3344758/phase_2_3/ItineraryInput.java
@@ -15,7 +15,7 @@ import org.apache.commons.lang3.StringUtils;
  * @author Jonathan Hill (d3344758)
  */
 public class ItineraryInput {
-  
+
   private String clientName;
   private String date;
   private int totalPeople;
@@ -32,7 +32,7 @@ public class ItineraryInput {
     readActivities.checkCreateFile();
     setActivityInformation(readActivities.readFile());
     setExsistingActivities(new String[getActivityInformation().size()]);
-    
+
     int counter = 0;  // for adding the items to the array
     for (String[] items : getActivityInformation()) {
       // adding just the activity name to a separate array
@@ -41,71 +41,69 @@ public class ItineraryInput {
     }
   }
 
-  // TODO Doc String  FIX REGEX
+  // TODO Doc String   --I really hate regex
   private void inputClientName(Scanner userInput) {
     boolean check = true;
     String checkHappy = "";
-    
+
     while (check) {
-      System.out.println("""
-                       Please enter the name of the client.  Enter as: First Initial space last name (ex: J Hill).
-                       Please use a maximum of 20 characters .""");
+      System.out.println("Please enter the name of the client. Enter as: "
+              + "First Initial space last name (ex: J Hill).");
+      System.out.println("Please use a maximum of 20 characters.");
       String name = userInput.nextLine();
-      
-      try {
-        String[] nameSplit = name.split(" ");
-        name = "";
-        for (int i = 0; i < nameSplit.length; i++) {
-          if (nameSplit[i].matches("^[a-zA-Z]")) {
-            name = name + StringUtils.capitalize(nameSplit[i]) + " ";
-          } else if (nameSplit[i].matches("\\d+")) {
-            System.out.println("Invalid input. Please do not use numbers.");
-            name = "";
-            break;
-          }
+
+      String[] nameSplit = name.trim().replaceAll("\\s+", " ").split(" ");
+      name = "";
+      boolean isValid = true;
+
+      for (String part : nameSplit) {
+        if (part.matches("^[a-zA-Z]*$")) {
+          name += StringUtils.capitalize(part) + " ";
+        } else {
+          System.out.println("Invalid input. Please do not use numbers.");
+          isValid = false;
+          break;
         }
-        
-        if (name.length() <= 20 && name.charAt(1) == ' ' && nameSplit.length >= 2) {
-          checkHappy = ValidationChecks.checkHappy(userInput, name);
-        } else if (name.length() > 20) {
-          System.out.println("Please ensure the name is less than 20 characters long. If it is more"
-                  + " Please shorten the last name.");
-        }
-      } catch (StringIndexOutOfBoundsException error) {
-        System.out.println("Please re-enter your name.");
       }
-      if (checkHappy.equals("yes")) {
-        check = false;
-        setClientName(name);
+
+      if (isValid && name.length() <= 20 && nameSplit.length >= 2) {
+        checkHappy = ValidationChecks.checkHappy(userInput, name);
+        if (checkHappy.equals("yes")) {
+          check = false;
+          setClientName(name.trim());
+        }
+      } else if (name.length() > 20) {
+        System.out.println("Please ensure the name is less than 20 characters long. "
+                + "If it is more, please shorten the last name.");
       }
     }
   }
 
-  // TODO JavaDoc maybe padding?
+  // TODO JavaDoc
   private void inputDate(Scanner userInput) {
     boolean check = true;
-    
+
     LocalDate today = LocalDate.now();
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     String formattedDate = today.format(formatter);
-    
+
     while (check) {
       String checkHappy = "";
-      
+
       System.out.printf("""
                         Please enter the date the activities are being booked for. Enter in the format: DD-MM-YYYY.
                         For Example todays date is: %s
                         """, formattedDate);
       String userDateInput = userInput.nextLine().trim();
-      
+
       try {
         LocalDate userDate = LocalDate.parse(userDateInput, formatter);
-        
+
         if (userDate.isBefore(today)) {
           System.out.println("The date entered is in the past. Please enter a future date.");
         } else {
           checkHappy = ValidationChecks.checkHappy(userInput, userDateInput);
-          
+
           if (checkHappy.equals("yes")) {
             System.out.println("The date is set for: " + userDate.format(formatter));
             check = false;
@@ -122,20 +120,20 @@ public class ItineraryInput {
   private void inputTotalPeople(Scanner userInput) {
     boolean check = true;
     int numberOfPeople = 0;
-    
+
     while (check) {
       String checkHappy = "";
       try {
         System.out.println("Please enter the amount of people in your party.");
         numberOfPeople = userInput.nextInt();
         userInput.nextLine();
-        
+
         if (numberOfPeople >= 1) {
           checkHappy = ValidationChecks.checkHappy(userInput, numberOfPeople);
         } else {
           System.out.println("Please ensure you are entering a number equal to or greater than 1");
         }
-        
+
         if (checkHappy.equals("yes")) {
           check = false;
           setTotalPeople(numberOfPeople);
@@ -154,7 +152,7 @@ public class ItineraryInput {
     String startLetters = RandomStringUtils.randomAlphabetic(2).toUpperCase();
     String numbers = RandomStringUtils.randomNumeric(3);
     String finalLetter = RandomStringUtils.randomAlphabetic(1).toUpperCase();
-    
+
     String reference = startLetters + numbers + finalLetter;
     setReferenceNumber(reference);
   }
@@ -164,7 +162,7 @@ public class ItineraryInput {
     boolean check = true;
     int totalActivities = getExsistingActivities().length;
     String checkHappy = "";
-    
+
     while (check) {
       for (int i = 0; i < totalActivities; i++) {
         System.out.print("Activity number " + i + ":  " + getExsistingActivities()[i] + "\t");
@@ -172,29 +170,29 @@ public class ItineraryInput {
           System.out.println("");
         }
       }
-      
+
       try {
         System.out.println("Please enter the activity you would like to add. "
                 + "Pick a number from the list provided.");
         int userChoice = userInput.nextInt();
         userInput.nextLine();
-        
+
         if (userChoice <= totalActivities && userChoice >= 0) {
           String userActivity = getExsistingActivities()[userChoice];
-          
+
           checkHappy = ValidationChecks.checkHappy(userInput, userActivity);
         } else {
           System.out.println("Please pick a number from the activities provided.");
         }
-        
+
         if (checkHappy.equals("yes")) {
           String[] activityInfo = getActivityInformation().get(userChoice);
           String code = activityInfo[1];
           activities.add(getExsistingActivities()[userChoice]);
           activityCodes.add(code);
-          
+
           check = ValidationChecks.addAnother(userInput, "Activity");
-          
+
         }
       } catch (InputMismatchException error) {
         System.out.println("Please enter a number that has been displayed.");
@@ -210,13 +208,13 @@ public class ItineraryInput {
     inputDate(userInput);
     inputActivities(userInput);
     clientRefernece();
-    
+
     generateReciept();
   }
 
   // TODO method
   private void generateReciept() {
-    
+
     System.out.println("+------------------------------+");
     System.out.printf("| %-20s%-5s |\n", "PH", "PH");
     System.out.println("+------------------------------+");
@@ -226,65 +224,65 @@ public class ItineraryInput {
   public String getClientName() {
     return clientName;
   }
-  
+
   public void setClientName(String clientName) {
     this.clientName = clientName;
   }
-  
+
   public String getDate() {
     return date;
   }
-  
+
   public void setDate(String date) {
     this.date = date;
   }
-  
+
   public int getTotalPeople() {
     return totalPeople;
   }
-  
+
   public void setTotalPeople(int totalPeople) {
     this.totalPeople = totalPeople;
   }
-  
+
   public String getReferenceNumber() {
     return referenceNumber;
   }
-  
+
   public void setReferenceNumber(String referenceNumber) {
     this.referenceNumber = referenceNumber;
   }
-  
+
   public List<String> getActivityCodes() {
     return activityCodes;
   }
-  
+
   public void setActivityCodes(List<String> activityCodes) {
     this.activityCodes = activityCodes;
   }
-  
+
   public int getTotalActivities() {
     return totalActivities;
   }
-  
+
   public void setTotalActivities(int totalActivities) {
     this.totalActivities = totalActivities;
   }
-  
+
   public String[] getExsistingActivities() {
     return exsistingActivities;
   }
-  
+
   public void setExsistingActivities(String[] exsistingActivities) {
     this.exsistingActivities = exsistingActivities;
   }
-  
+
   public List<String[]> getActivityInformation() {
     return activityInformation;
   }
-  
+
   public void setActivityInformation(List<String[]> activityInformation) {
     this.activityInformation = activityInformation;
   }
-  
+
 }
